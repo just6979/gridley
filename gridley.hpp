3b1787c78b5031f761e0f3489bf0d15efbc317db
Justin White <just6979@yahoo.com>
2009-03-22 02:31:21 -0400

    Completed port to SDL, added CodeBlocks project file.

---
+++ gridley.hpp
@@ -0,0 +1,82 @@
+/* Various declarations for the Gridley project */
+
+#ifndef GRIDLEY_HPP_INCLUDED
+#define GRIDLEY_HPP_INCLUDED
+
+/* constants */
+#define MAX_POINTS 1000
+#define MAX_LEN 80
+const GLfloat PI = 3.14159265;
+const GLint GRID = 1;
+const GLint OVERLAY = 2;
+
+/* types */
+
+// store world coords and line endpoint status
+struct LinePoint {
+	GLfloat x;
+	GLfloat y;
+	GLboolean end;
+};
+
+// 2D integer point
+typedef struct {
+	GLint x;
+	GLint y;
+} Point2i;
+
+// 3 by 3 matrix
+typedef GLfloat Matrix3x3[3][3];
+
+// mouse state: 2D position and 5 buttons
+#define MAX_MOUSE_BUTTONS 5
+struct mouse_state {
+	Point2i pos;
+	GLboolean button[MAX_MOUSE_BUTTONS];
+};
+
+// other globals, keep them in this struct to prevent strays
+struct Globals {
+	// framebuffer dimensions
+	GLint width, height, bpp;
+	// ranges of the origin centered ortho projection
+	GLfloat x_range, y_range;
+	// how many points to skip between gridlines
+	GLint grid_scale;
+	// how many pixels between each point
+	GLfloat screen_scale;
+	// array of chosen points
+	LinePoint points[MAX_POINTS];
+	// index to last point, the one that will be picked next
+	GLint last_point;
+	// factor to perform each transform by
+	GLfloat factor;
+	// keep track of digits for entering factors
+	GLint factor_index;
+	// how many points to display in status
+	GLint display_points;
+	// mouse state
+	mouse_state mouse;
+	// framebuffer (or backbuffer if doublebuffered) surface
+	SDL_Surface* screen;
+	// DejaVu Sans TrueType font
+	TTF_Font* font;
+	SDL_Color font_color;
+	GLint font_height;
+	// flags
+	GLboolean show_help;
+	GLboolean show_cur_line;
+	GLboolean snap_to_grid;
+	GLboolean show_grid;
+	GLboolean show_status;
+	GLboolean fullscreen;
+	GLboolean keep_factor;
+	// is the event loop still going?
+	GLboolean running;
+} g;
+
+
+/* functions */
+int quit(int = 0);
+
+#endif // GRIDLEY_HPP_INCLUDED
